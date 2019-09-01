ede874a764ce8407cb9d570210ed21f1689f4d12
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-01 22:52:43 +0300

    Listing arguments using new keyword

--- contact.py
+++ contact.py
@@ -3,4 +3,14 @@ class User:
     This class will generate new instance of the user
     '''
 
-    def __init__(self,fname,lname,userName,password):
+    def __init__(self,fname,lname,userName,psword):
+
+        '''
+        __init__method aid in defining the properties of the objects
+        '''
+
+        Args:
+            fname: New user first name
+            lname: New user last name
+            userName: New user user name
+            psw: New user passwod
