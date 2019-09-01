a289102744dccc21d0a0e623e89aa3748ba66c09
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-01 23:11:42 +0300

    Providing subclass and unittest for testing class behavior

--- user_test.py
+++ user_test.py
@@ -1,2 +1,9 @@
 import unittest
 from user import User
+
+class TestIUser(unittest.TestCase):
+    '''
+    Thisntests class that defines test cases for the user class characteristics.
+    Args:
+    unittest.TestCase:TestCase class is a subclass for creating test cases
+    '''
