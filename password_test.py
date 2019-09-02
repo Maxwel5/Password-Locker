147c3aa3e78d64caae5924fb4d4c7126b51ef66c
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-02 14:53:26 +0300

    Creating a test case that test if we canfind a user object

--- password_test.py
+++ password_test.py
@@ -54,20 +54,41 @@ class TestIUser(unittest.TestCase):
             objects to our user_list
             '''
             self.new_user.save_user()
-            test_user = User("Test","user","9876","Max") 
+            test_user = User("Test","user","Max","9876") 
             test_user.save_user()
             self.assertEqual(len(User.user_list),2)
 
-        def test_delete_user(self):
-            '''
-            test_delete_user to try if we can delete a user from our user list
-            '''
-            self.new_user.save_user()
-            test_user = User("Test","user","9876","Max")
-            test_user.save_user()
+    def test_delete_user(self):
+        '''
+        test_delete_user to try if we can delete a user from our user list
+        '''
+        self.new_user.save_user()
+        test_user = User("Test","user","Max","9876")
+        test_user.save_user()
 
-            self.new_user.delete_user()
-            self.assertEqual(len(User.user_list),1)
+        self.new_user.delete_user()
+        self.assertEqual(len(User.user_list),1)
+
+    def delete_user(self):
+
+        '''
+        delete_user method removes/deletes a saved user from the user_list
+        '''
+
+        User.user_list.remove(self)
+    
+    def test_find_user_by_username(self):
+        '''
+        test to check if we can find a user by username and display information
+        '''
+
+        self.new_user.save_user()
+        test_user = User("Test","user","Max","9876") 
+        test_user.save_user()
+
+        found_user = User.find_by_username("Max")
+
+        self.assertEqual(found_user.password,test_user.password)
 
 if __name__ ==  '__main__':
     unittest.main()
