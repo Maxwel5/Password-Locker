964419c8518ea8847ff987acde975ba9de3e82d9
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-02 15:20:32 +0300

    Testing for user display

--- password_test.py
+++ password_test.py
@@ -133,6 +133,13 @@ class TestIUser(unittest.TestCase):
 
         return False
 
+    def test_display_all_users(self):
+        '''
+        method that returns a list of all users saved
+        '''
+
+        self.assertEqual(User.display_users(),User.user_list)
+
 if __name__ ==  '__main__':
     unittest.main()
 
